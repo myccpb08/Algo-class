423ee6c0178c193bce9c806c9b331e704267853b
YR <myccpb08@gmail.com>
2019-03-05 21:34:54 +0900

    DAY1

---
+++ 수업/Day13 우선순위큐 이진트리 힙/숙제/hw3.사칙연산.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('사칙연산.txt', 'r')
+
+# 자식이 있으면, 자기는 연산자
+# 자식이 없으면, 자기는 숫자
+
+for i in range(1,11):
+    n=int(input())
+    flag=1
+    for j in range(n):
+        info=input().split()
+        if len(info)>2:   # 자식이 있다는 의미
+            if not info[1] in ['-','+','*','/']:
+                flag=0
+        else:
+            if info[1] in ['-','+','*','/']:
+                flag=0
+    print('#{} {}'.format(i,flag))
+
+
+
